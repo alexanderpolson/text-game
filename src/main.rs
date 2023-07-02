2c0f0ba918fd3fc4daaa03df15ff14e4f58e22cf
Alexander Polson <alex@alexpolson.com>
2023-07-02 12:02:17 -0700

    Added a main menu to determine if a new or existing map should be created.

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,14 @@ Current Location:
              , location.element, possible_directions);
 }
 
+fn main_menu() -> String {
+    println!(r#"
+1. New Map
+2. Load Existing Map
+x. Exit"#);
+    return prompt_with_options(PROMPT, vec!["1", "2", "x"]);
+}
+
 fn location_edit_menu(graph: &StringGraph) -> String {
     print_current_location(graph);
     println!(r#"
@@ -54,7 +62,7 @@ fn location_edit_menu(graph: &StringGraph) -> String {
 3. Move
 4. Enter interactive mode
 5. Reset to root node.
-x. Exit"#);
+x. Back to the main menu"#);
     return prompt_with_options(PROMPT, vec!["1", "2", "3", "4", "5", "x"]);
 }
 
@@ -75,12 +83,10 @@ fn connect_location(graph: &mut StringGraph) {
         match prompt_with_options("Create a new location or use an existing location?", vec!["N", "n", "E", "e"]).as_str() {
             "n" | "N" => {
                 graph.insert_edge_to_new_node(new_direction.clone(), prompt("Enter the description for the new location:"));
-                save(graph);
                 break;
             }
             "e" | "E" => {
                 graph.insert_edge_to_existing_node(new_direction.clone(), select_node(graph));
-                save(graph);
                 break;
             }
             _ => (),
@@ -95,7 +101,6 @@ fn connect_location(graph: &mut StringGraph) {
                 let return_direction = prompt("Enter the return direction that will take you back:");
                 let original_node_borrowed = original_node.borrow();
                 graph.current_node().borrow_mut().insert_edge(return_direction, original_node_borrowed.id.clone());
-                save(graph);
                 break;
             }
             "n" | "N" => break,
@@ -154,10 +159,10 @@ fn interactive_mode(graph: &mut StringGraph) {
     }
 }
 
-fn save(graph: &StringGraph) {
-    match serde_json::to_string(&graph) {
+fn save(graph_file: &GraphFile) {
+    match serde_json::to_string(&graph_file.graph) {
         Ok(data) => {
-            match fs::write("game.json", data.as_bytes()) {
+            match fs::write(&graph_file.file_name, data.as_bytes()) {
                 Ok(_) => (),
                 Err(e) => println!("An error occurred: {:#?}", e)
             }
@@ -166,24 +171,52 @@ fn save(graph: &StringGraph) {
     }
 }
 
+struct GraphFile {
+    graph: StringGraph,
+    file_name: String,
+}
+
 fn main() {
     // TODO: Allow reading/writing specific files by adding an initial menu before the one below.
-    let mut graph: StringGraph = match fs::read_to_string("game.json") {
-        Ok(graph_data) => serde_json::from_str(graph_data.as_str()).expect("Issue deserializing game data."),
-        Err(_e) => StringGraph::new(prompt("Enter the description of your first node:")),
-    };
+
     loop {
-        match location_edit_menu(&graph).as_str() {
-            "1" => update_location_description(&mut graph),
-            "2" => connect_location(&mut graph),
-            "3" => {
-                move_to_location(&mut graph);
-                ()
+        let mut graph_file = match main_menu().as_str() {
+            "1" => {
+                GraphFile {
+                    file_name: prompt("Enter the file name for your graph:"),
+                    graph: StringGraph::new(prompt("Enter the description of your first node:")),
+                }
             }
-            "4" => interactive_mode(&mut graph),
-            "5" => graph.reset(),
+            "2" => {
+                let file_name = prompt("Enter the file name for your graph:");
+                let graph_data = fs::read_to_string(&file_name).expect("Issue loading game data.");
+                GraphFile {
+                    graph: serde_json::from_str(graph_data.as_str()).expect("Issue deserializing game data."),
+                    file_name: file_name,
+                }
+            }
             "X" | "x" => break,
-            _ => ()
+            _ => continue
+        };
+        loop {
+            match location_edit_menu(&graph_file.graph).as_str() {
+                "1" => {
+                    update_location_description(&mut graph_file.graph);
+                    save(&graph_file)
+                }
+                "2" => {
+                    connect_location(&mut graph_file.graph);
+                    save(&graph_file)
+                }
+                "3" => {
+                    move_to_location(&mut graph_file.graph);
+                    ()
+                }
+                "4" => interactive_mode(&mut graph_file.graph),
+                "5" => graph_file.graph.reset(),
+                "X" | "x" => break,
+                _ => ()
+            }
         }
     }
 }
